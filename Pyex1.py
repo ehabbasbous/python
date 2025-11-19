b0fcab9894e88b1714c819acd0025cf76d5d7055
ehabbasbous <ehabbasbous@gmail.com>
2025-11-19 02:01:58 +0100

    Add files via upload

---
+++ Pyex1.py
@@ -0,0 +1,15 @@
+age=30
+print("my age is:",age)
+name= "Ehab"
+print("My name is", name)
+fruits=["apple","banana", "mango","blueberry","orange"]
+print(fruits[1])
+y="123"
+print(int(y),10)
+print(int(y)+10)
+
+from math import sqrt
+print( sqrt( 16 ) )
+
+from math import sqrt as squareroot
+print( squareroot( 16 ) ) 
